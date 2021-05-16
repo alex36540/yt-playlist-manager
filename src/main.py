22c0dd7036f18fd677ee4afbe252b106994dbcb0
alex36540 <alex36540@gmail.com>
2021-05-16 01:26:02 -0400

    created get_videos function

--- src/main.py
+++ src/main.py
@@ -1,11 +1,24 @@
 import pyyoutube
-from private import __API_KEY
+import private
 
-API_KEY = __API_KEY
+API_KEY = private.API_KEY
+api = pyyoutube.Api(api_key=API_KEY)
+
+
+def get_videos():
+    videos = []
+    pl_id = private.PLAYLIST_ID
+
+    pl_items = api.get_playlist_items(playlist_id=pl_id, count=None, limit=50)
+
+    for item in pl_items.items:
+        videos.append({item.snippet.resourceId.videoId: item.snippet.title})  # ID: title
+
+    return videos
 
 
 def main() -> None:
-    print("gg")
+    print(get_videos())
 
 
 if __name__ == '__main__':
