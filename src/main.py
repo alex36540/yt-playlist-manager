0c5ddf88d4b90d0c2227af012d34d05690a901fd
alex36540 <alex36540@gmail.com>
2021-05-16 17:00:12 -0400

    wrote write_videos and load_videos as well as updated documentation

--- src/main.py
+++ src/main.py
@@ -1,13 +1,24 @@
-import pyyoutube
-import private
-import videos
+"""
+File: main.py
+Author: Alexander Lee (https://github.com/alex36540)
 
-API_KEY = private.API_KEY
+Description: Runs the playlist manager.
+"""
+import pyyoutube
+from private import *
+import video
+
 api = pyyoutube.Api(api_key=API_KEY)
 
 
 def main() -> None:
-    print(videos.get_videos(api))
+    playlist = api.get_playlist_by_id(playlist_id=PLAYLIST_ID).items[0]
+    name = playlist.snippet.title
+
+    videos = video.get_videos(api, PLAYLIST_ID)
+
+    video.write_videos(videos, name)
+    print(video.load_videos(name))
 
 
 if __name__ == '__main__':
