3e751bb736e8225b27eb73d2f27f71eb099aa2b2
alex36540 <alex36540@gmail.com>
2021-05-18 21:00:06 -0400

    added models

--- src/mysite/playlist_manager/models.py
+++ src/mysite/playlist_manager/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Playlist(models.Model):
+    id = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    # TODO add videos field
+
+
+class Video(models.Model):
+    id = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
+    index = models.IntegerField()
