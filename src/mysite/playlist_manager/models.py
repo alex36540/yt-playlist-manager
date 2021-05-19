ef122238fbcbc808c9f600e2a655abd1018de005
alex36540 <alex36540@gmail.com>
2021-05-18 22:10:24 -0400

    updated models to include 'to string'

--- src/mysite/playlist_manager/models.py
+++ src/mysite/playlist_manager/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 
 class Playlist(models.Model):
-    id = models.CharField(max_length=200)
+    id = models.CharField(max_length=200, primary_key=True)
     name = models.CharField(max_length=200)
     # TODO add videos field
+    # this is also where you add methods
+
+    def __str__(self):
+        return self.name
 
 
 class Video(models.Model):
-    id = models.CharField(max_length=200)
+    id = models.CharField(max_length=200, primary_key=True)
     title = models.CharField(max_length=200)
     playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
     index = models.IntegerField()
+
+    def __str__(self):
+        return str(self.index) + " " + str(self.title)
