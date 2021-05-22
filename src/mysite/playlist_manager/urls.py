97aad204267e85bed8c5ff656f6570ea5032257d
alex36540 <alex36540@gmail.com>
2021-05-22 00:41:50 -0400

    added playlist url

--- src/mysite/playlist_manager/urls.py
+++ src/mysite/playlist_manager/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<str:playlist_id>/', views.playlist, name='playlist')
 ]
