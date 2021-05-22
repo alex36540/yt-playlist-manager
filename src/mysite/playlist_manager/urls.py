87fd64b5b0519fbc24ff210b3e9e664de92046a5
alex36540 <alex36540@gmail.com>
2021-05-22 18:50:21 -0400

    added app name

--- src/mysite/playlist_manager/urls.py
+++ src/mysite/playlist_manager/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 
+app_name = 'playlist_manager'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<str:playlist_id>/', views.playlist, name='playlist')
