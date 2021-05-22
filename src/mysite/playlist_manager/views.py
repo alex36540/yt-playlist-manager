cd380000bc75a897d6abc11d42f084e1fcb22ae6
alex36540 <alex36540@gmail.com>
2021-05-22 00:42:23 -0400

    added playlist view

--- src/mysite/playlist_manager/views.py
+++ src/mysite/playlist_manager/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Playlist, Video
 
 
 def index(request):
     return HttpResponse("Eyyyy waddup jit")
+
+
+def playlist(request):
+    return HttpResponse("Playlisttt")
