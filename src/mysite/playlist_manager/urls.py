ca85c7427880905021ddf29756bdcc4c267a5837
alex36540 <alex36540@gmail.com>
2021-05-23 15:10:26 -0400

    modified playlist url

--- src/mysite/playlist_manager/urls.py
+++ src/mysite/playlist_manager/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'playlist_manager'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:playlist_id>/', views.playlist, name='playlist')
+    path('playlist/<str:playlist_id>/', views.playlist, name='playlist')
 ]
